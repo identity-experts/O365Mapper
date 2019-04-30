1dd4b4dd46aa9f928503669198a6fb44b40c0a7f
Nitin Khubani <nitink@identityexperts.co.uk>
2019-04-30 10:19:04 +0400

    Version changing from 1.0.0.3 to 1.0.0.4

--- wpf-notifyicon/wpf-notifyicon/NotifyIconWpf/Properties/AssemblyInfo.cs
+++ wpf-notifyicon/wpf-notifyicon/NotifyIconWpf/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Windows.Markup;
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 
 
 //provides simplified declaration in XAML
